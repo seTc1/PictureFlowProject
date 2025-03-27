917d1ff313b981691807fc036c01b757cdd7d220
seTc1 <sema2234.archi@gmail.com>
2025-03-27 18:31:26 +0300

    [0.2] App Tests

---
+++ PictureFlow/data/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
---
+++ PictureFlow/main.py
@@ -0,0 +1,26 @@
+from doctest import debug
+
+from flask import Flask, render_template
+from data import db_session
+
+app = Flask(__name__)
+with open('API_KEY.api', 'r') as file:
+    API_KEY = file.readline().strip()
+app.config['SECRET_KEY'] = API_KEY
+
+
+def main():
+    app.run(debug=True)
+
+
+@app.route('/')
+@app.route('/main')
+def base():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    main()
