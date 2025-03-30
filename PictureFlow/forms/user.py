8dd1457b6e2707cd2457a6623e17b33468c3ae39
seTc1 <sema2234.archi@gmail.com>
2025-03-30 22:15:58 +0300

    [0.7] WTF is wrong

--- PictureFlow/forms/user.py
+++ PictureFlow/forms/user.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
 
 
@@ -8,4 +8,10 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     name = StringField('Имя пользователя', validators=[DataRequired()])
+    submit = SubmitField('Зарегестрироваться')
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
--- PictureFlow/main.py
+++ PictureFlow/main.py
@@ -3,7 +3,7 @@ from doctest import debug
 from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 from data import db_session
 from data.users import User
 
@@ -11,16 +11,40 @@ app = Flask(__name__)
 with open('API_KEY.api', 'r') as file:
     API_KEY = file.readline().strip()
 app.config['SECRET_KEY'] = API_KEY
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 def main():
     db_session.global_init("db/users.db")
     app.run(debug=True)
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 @app.route('/')
 def index():
     return render_template('main.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+
+        users = db_sess.query(User).all()
+        print("\n=== Список пользователей в базе данных ===")
+        for u in users:
+            print(u)
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
@@ -42,8 +66,26 @@ def reqister():
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
         return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
+
+@app.route('/profile')
+def profile():
+    if current_user.is_authenticated:
+        return redirect('/')
+    else:
+        return redirect('/register')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 @app.errorhandler(404)
 def page_not_found(error):
