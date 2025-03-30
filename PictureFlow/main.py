12f94f0d97d962f187c6c2b1460d58b66729edb8
seTc1 <sema2234.archi@gmail.com>
2025-03-30 20:57:36 +0300

    [0.6] RegisterTest

--- PictureFlow/main.py
+++ PictureFlow/main.py
@@ -1,7 +1,11 @@
 from doctest import debug
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, abort
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+from forms.user import RegisterForm
 from data import db_session
+from data.users import User
 
 app = Flask(__name__)
 with open('API_KEY.api', 'r') as file:
@@ -18,10 +22,28 @@ def main():
 def index():
     return render_template('main.html')
 
-@app.route('/login')
-def login():
-    return render_template('register.html')
-
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
 
 @app.errorhandler(404)
 def page_not_found(error):
