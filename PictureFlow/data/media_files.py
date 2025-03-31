08e0c44882fbad240755ef8667c069c30466c0b9
seTc1 <sema2234.archi@gmail.com>
2025-03-31 23:16:12 +0300

    [0.9] Authorization Complete

--- PictureFlow/data/media_files.py
+++ PictureFlow/data/media_files.py
@@ -1,4 +1,6 @@
 import datetime
+from enum import unique
+
 import sqlalchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,11 +11,12 @@ from .db_session import SqlAlchemyBase
 class Media(SqlAlchemyBase, UserMixin):
     __tablename__ = 'media_files'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    url = sqlalchemy.Column(sqlalchemy.String, primary_key=True, unique=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    autor = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hiden = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
     def __repr__(self):
-        return f'<User> {self.id} {self.name} {self.email}'
+        return f'<Media> {self.url} {self.name}'
--- PictureFlow/main.py
+++ PictureFlow/main.py
@@ -14,9 +14,7 @@ app.config['SECRET_KEY'] = API_KEY
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-def main():
-    db_session.global_init("db/users.db")
-    app.run(debug=True)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -25,61 +23,75 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    username = None
+    if current_user.is_authenticated:
+        username = current_user.name
+        print(current_user)
+    return render_template('main.html', registered=current_user.is_authenticated, username=username)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        return redirect('/profile')
+    else:
+        form = LoginForm()
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
 
-        users = db_sess.query(User).all()
-        print("\n=== Список пользователей в базе данных ===")
-        for u in users:
-            print(u)
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
+            users = db_sess.query(User).all()
+            print("\n=== Список пользователей в базе данных ===")
+            for u in users:
+                print(u)
+            user = db_sess.query(User).filter(User.email == form.email.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                return redirect("/")
+            return render_template('login.html',
+                                   message="Неправильный логин или пароль",
+                                   form=form)
+        return render_template('login.html', title='Авторизация', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            email=form.email.data,
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return redirect('/')
-    return render_template('register.html', title='Регистрация', form=form)
+    if current_user.is_authenticated:
+        return redirect('/profile')
+    else:
+        form = RegisterForm()
+        if form.validate_on_submit():
+            if form.password.data != form.password_again.data:
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Пароли не совпадают")
+            if len(form.name.data) > 16:
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Слишком длинное имя (> 16)")
+            db_sess = db_session.create_session()
+            if db_sess.query(User).filter(User.email == form.email.data).first():
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Такой пользователь уже есть")
+            user = User(
+                name=form.name.data,
+                email=form.email.data,
+            )
+            user.set_password(form.password.data)
+            db_sess.add(user)
+            db_sess.commit()
+            db_sess = db_session.create_session()
+            user = db_sess.query(User).filter(User.email == form.email.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                return redirect("/")
+            return redirect('/')
+        return render_template('register.html', title='Регистрация', form=form)
 
 @app.route('/profile')
+@login_required
 def profile():
     if current_user.is_authenticated:
         return redirect('/')
-    else:
-        return redirect('/register')
+
 
 @app.route('/logout')
 @login_required
@@ -90,6 +102,11 @@ def logout():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+def main():
+    db_session.global_init("db/users.db")
+    db_session.global_init("db/media_files.db")
+    app.run(debug=True)
 
 if __name__ == '__main__':
     main()
